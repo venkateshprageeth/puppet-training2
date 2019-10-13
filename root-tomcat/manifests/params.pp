7803136b938cc89a676c33cda1f3edd6bd1cc27b
root <root@jenkins-master.us-central1-a.c.devops-training-245521.internal>
2019-10-13 14:03:22 +0000

    check

--- root-tomcat/manifests/params.pp
+++ root-tomcat/manifests/params.pp
@@ -2,7 +2,7 @@ class root-tomcat::params {
         $pack = ['tomcat','tomcat-webapps']
         $ser  = 'tomcat'
         $ser_status = 'running'
-        $tomcat_cfg_loaded = '2'
+        $tomcat_cfg_loaded = '3'
         $config_path = '/etc/tomcat/tomcat.conf'
         $tomcat_base = '/var/lib/tomcat'
         $Xms = '100m'
